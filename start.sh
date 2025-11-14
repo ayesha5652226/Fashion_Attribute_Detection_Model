82de7a829dfd0eeb6ebeb48a80a46e68c25b1036
ayesha <ayeshabegum91314@gmail.com>
2025-11-14 15:24:40 +0530

    Make start.sh executable

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
-#!/usr/bin/env bash
-set -o errexit
-
-# unzip model if needed
-if [ -f "model/model_quantized_scripted.zip" ] && [ ! -f "model/model_quantized_scripted.pt" ]; then
-  unzip model/model_quantized_scripted.zip -d model/
-fi
-
-# start fastapi
-uvicorn app:app --host 0.0.0.0 --port $PORT
+#!/bin/bash
+gunicorn app:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
